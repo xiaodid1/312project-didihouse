af39015e6b67f2866bb80139313058ba11216235
michelle1565 <michelle164650@gmail.com>
2023-10-17 13:08:50 -0400

    part 1 without docker

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 import os.path
-
+import bcrypt
+import string
+import random
+from pymongo import MongoClient
 from flask import Flask, render_template, send_from_directory, request, make_response
 
 app = Flask(__name__)
 
+mongo_client = MongoClient("mongo")
+db = mongo_client["CSE312_Project"]
+post_collection = db["Posts"]
+post_collection.insert_one({'use': 'counter', 'value': 0})
 
 @app.route("/")
 def index():
+    if request.cookies.get('token') and post_collection.find_one({"token": request.cookies.get('token')}):
+        user = post_collection.find_one({"token": request.cookies.get('token')})['username']
+        render_template("index.html", welcome = f"Welcomeback to React Post App, {user}")
+    else:
+        render_template("index.html", welcome = "Welcome to React Post App" )
     resp = app.send_static_file("index.html")
     response = make_response(resp, 200)
     response.headers = {'content-type': 'text/html; charset=UTF-8', 'x-content-type-options': 'nosniff'}
@@ -54,6 +66,47 @@ def count():
     response.set_cookie('visited', str(visit), max_age=3600)
 
     return response
+
+@app.route('/registration', method = 'POST')
+def registration():
+    username = request.form['username_reg']
+    password = request.form['password_reg']
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password, salt)
+    post_collection.insert_one({'username': username, 'hashed': hashed})
+    render_template("message.html", response_message = "User Register")
+    resp = app.send_static_file("message.html")
+    response = make_response(resp, 200)
+    return response
+
+@app.route('/login', method = 'POST')
+def login():
+    username = request.form['username_login']
+    password = request.form['password_login']
+    cflag = False
+    if post_collection.find_one({"username": username}):
+        user = post_collection.find_one({"username": username})
+        hashed = user['hashed']
+        if bcrypt.checkpw(password, hashed):
+            token = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+            hashedtoken = hash(token)
+            post_collection.update_one({"username": username}, {"$set": {"token": hashedtoken}})
+            render_template("message.html", response_message = "User Login")
+            cflag = True
+        else:
+            render_template(index.html, response_message = "login Error")           
+    else:
+        render_template(index.html, response_message = "login Error")  
+
+    resp = app.send_static_file("message.html")
+    response = make_response(resp, 200)
+    
+    if cflag:
+        response.set_cookie('token', token, max_age=3600)
+
+    return response          
+    
+
 
 
 if __name__ == '__main__':
