b601146fcd6070d03d4338879d19bd0475972745
Mingfeng Mai <mingf.m04@gmail.com>
2023-12-04 07:28:20 -0500

    part4 lo2
    part4 lo2 testing

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('https://auction404notfound.com', { transports : ['websocket']});
 
 export default socket;
